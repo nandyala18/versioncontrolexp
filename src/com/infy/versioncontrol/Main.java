af45440dd1cfec8651a4dbb8661ae14ad599d0ca
seshu <n.seshu.babu@gmail.com>
2019-10-28 19:15:19 +0530

    initial git commit with java project

---
+++ src/com/infy/versioncontrol/Main.java
@@ -0,0 +1,9 @@
+package com.infy.versioncontrol;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        system.out.println("This Is....")
+    }
+}
